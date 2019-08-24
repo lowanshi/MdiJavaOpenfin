828c2819d89c9acbdf6acf8da8feeb0d87da039c
Praveen Lowanshi <lowanshi@hotmail.com>
2019-08-24 20:22:07 +0530

    minimize and restore fix using reflection for frame where we can't override JInternalFrame

--- src/main/java/com/openfin/desktop/demo/DesktopFrame.java
+++ src/main/java/com/openfin/desktop/demo/DesktopFrame.java
@@ -4,26 +4,41 @@ import com.openfin.desktop.*;
 import com.openfin.desktop.Window;
 import com.sun.jna.Native;
 
+import javax.accessibility.AccessibleContext;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
+
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyVetoException;
+import java.beans.VetoableChangeListener;
 import java.lang.System;
+import java.lang.reflect.Field;
 import java.util.UUID;
+import javax.swing.plaf.*;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+import static javax.swing.plaf.basic.BasicInternalFrameTitlePane.*;
 
 /**
  * Created by Praveen Lowanshi on 2/24/2019.
  */
-public class DesktopFrame extends JFrame 
+public class DesktopFrame extends JFrame
 {
    private JDesktopPane theDesktop;
    private String openfin_app_url = "https://www.google.com";
 
    protected DesktopConnection desktopConnection;
 
-   public DesktopFrame()
-   {
-      super( "Using a JDesktopPane" );
+   public DesktopFrame() {
+      super("Using a JDesktopPane");
       try {
          this.desktopConnection = new DesktopConnection(appUuid);
       } catch (DesktopException desktopError) {
@@ -35,29 +50,119 @@ public class DesktopFrame extends JFrame
 
       startOpenFinRuntime();
       JMenuBar bar = new JMenuBar(); // create menu bar
-      menu=new JMenu("Menu");
-      submenu=new JMenu("Sub Menu");
-      i1=new JMenuItem("Item 1");
-      i2=new JMenuItem("Item 2");
-      i3=new JMenuItem("Item 3");
-      i4=new JMenuItem("Item 4");
-      i5=new JMenuItem("Item 5");
-      menu.add(i1); menu.add(i2); menu.add(i3);
-      submenu.add(i4); submenu.add(i5);
+      menu = new JMenu("Menu");
+      submenu = new JMenu("Sub Menu");
+      i1 = new JMenuItem("Item 1");
+      i2 = new JMenuItem("Item 2");
+      i3 = new JMenuItem("Item 3");
+      i4 = new JMenuItem("Item 4");
+      i5 = new JMenuItem("Item 5");
+      menu.add(i1);
+      menu.add(i2);
+      menu.add(i3);
+      submenu.add(i4);
+      submenu.add(i5);
       menu.add(submenu);
       bar.add(menu);
-      setJMenuBar( bar ); // set menu bar for this application
+      setJMenuBar(bar); // set menu bar for this application
 
       embedCanvas = new java.awt.Canvas();
+      embedCanvas.setFocusable(true);
 
-      JInternalFrame frame = new JInternalFrame(
-              "Internal Frame", true, true, true, true );
+      //create a hidden frame to hold the openfin window when it's iconified.
+      hiddenFrame = new JFrame();
+      hiddenFrame.setPreferredSize(new Dimension(640, 480));
+      hiddenFrame.pack();
+      hiddenFrame.setVisible(false);
 
-      frame.add( embedCanvas, BorderLayout.CENTER );
-      frame.setSize(1000,1000);
+      frame = new JInternalFrame("Internal Frame", true, true, true, true);
+
+      BasicInternalFrameTitlePane titlePane = (BasicInternalFrameTitlePane) ((BasicInternalFrameUI) frame.getUI()).getNorthPane();
+      // Class clsTitlePane = titlePane.class.getDeclaredField();
+
+      frame.add(embedCanvas, BorderLayout.CENTER);
+      frame.setSize(1000, 1000);
       theDesktop = new JDesktopPane(); // create desktop pane
-      theDesktop.add( frame ); // attach internal frame
-      frame.setVisible( true ); // show internal frame
+      theDesktop.add(frame); // attach internal frame
+      frame.pack();
+      frame.setVisible(true); // show internal frame
+      frame.setIconifiable(false);
+      //when mixing lightweight and heavyweight component
+      frame.addComponentListener(new ComponentListener() {
+         private void revalidateWorkaround() {
+            theDesktop.revalidate();
+            DesktopFrame.this.validate();
+         }
+
+         @Override
+         public void componentResized(ComponentEvent e) {
+            revalidateWorkaround();
+         }
+
+         @Override
+         public void componentMoved(ComponentEvent e) {
+            revalidateWorkaround();
+         }
+
+         @Override
+         public void componentShown(ComponentEvent e) {
+            revalidateWorkaround();
+         }
+
+         @Override
+         public void componentHidden(ComponentEvent e) {
+            revalidateWorkaround();
+         }
+      });
+      frame.addInternalFrameListener(new InternalFrameAdapter() {
+         @Override
+         public void internalFrameDeiconified(InternalFrameEvent e) {
+            //when internal frame is restored, re-embed openfin window back to canvas
+            long canvasHWndId = Native.getComponentID(embedCanvas);
+            if (startupHtml5app != null) {
+               startupHtml5app.getWindow().embedInto(canvasHWndId, embedCanvas.getWidth(), embedCanvas.getHeight(), new AckListener() {
+                  @Override
+                  public void onSuccess(Ack ack) {
+                  }
+
+                  @Override
+                  public void onError(Ack ack) {
+                  }
+               });
+            }
+         }
+      });
+      try {
+         Field f1 = BasicInternalFrameTitlePane.class.getDeclaredField("iconifyAction");
+         f1.setAccessible(true);
+         AbstractAction myAction = new BTSInofied(frame);
+         Field iconButton = BasicInternalFrameTitlePane.class.getDeclaredField("iconButton");
+         iconButton.setAccessible(true);
+
+         NoFocusButton btsIconButton = new NoFocusButton(
+                 "InternalFrameTitlePane.iconifyButtonAccessibleName",
+                 "InternalFrameTitlePane.iconifyButtonOpacity");
+         btsIconButton.addActionListener(myAction);
+         titlePane.add(btsIconButton);
+         iconButton.set(titlePane, btsIconButton);
+         Icon iconIcon = UIManager.getIcon("InternalFrame.iconifyIcon");
+         btsIconButton.setIcon(iconIcon);
+         btsIconButton.setToolTipText("Minimize");
+         Boolean paintActive = frame.isSelected() ? Boolean.TRUE : Boolean.FALSE;
+         btsIconButton.putClientProperty("paintActive", paintActive);
+         Border handyEmptyBorder = new EmptyBorder(0, 0, 0, 0);
+
+         btsIconButton.setBorder(handyEmptyBorder);
+         btsIconButton.setContentAreaFilled(false);
+         myAction.setEnabled(true);
+         f1.set(titlePane, myAction);
+         myAction.setEnabled(true);
+      } catch (NoSuchFieldException e) {
+         e.printStackTrace();
+      } catch (IllegalAccessException e) {
+         e.printStackTrace();
+      }
+
 
       embedCanvas.addComponentListener(new ComponentAdapter() {
          @Override
@@ -66,7 +171,7 @@ public class DesktopFrame extends JFrame
             Dimension newSize = event.getComponent().getSize();
             try {
                if (startupHtml5app != null) {
-                  startupHtml5app.getWindow().embedComponentSizeChange((int)newSize.getWidth(), (int)newSize.getHeight());
+                  startupHtml5app.getWindow().embedComponentSizeChange((int) newSize.getWidth(), (int) newSize.getHeight());
                }
             } catch (Exception e) {
                e.printStackTrace();
@@ -75,8 +180,8 @@ public class DesktopFrame extends JFrame
       });
 
 
-       add( theDesktop ); // add desktop pane to frame
-    
+      add(theDesktop); // add desktop pane to frame
+
    } // end constructor DesktopFrame
 
    private void launchHTMLApps() {
@@ -84,37 +189,38 @@ public class DesktopFrame extends JFrame
       int width = 300, height=200;
       int gap = 50;  // gap between windows at initial positions
 
-         try {
-            String uuid = UUID.randomUUID().toString();
-            ApplicationOptions options = new ApplicationOptions(uuid, uuid, openfin_app_url);
-            WindowOptions mainWindowOptions = new WindowOptions();
-            mainWindowOptions.setAutoShow(true);
-            mainWindowOptions.setDefaultHeight(height);
-            mainWindowOptions.setDefaultTop(50);
-            mainWindowOptions.setDefaultWidth(width);
-            mainWindowOptions.setShowTaskbarIcon(true);
-            mainWindowOptions.setSaveWindowState(false);  // set to false so all windows start at same initial positions for each run
-            options.setMainWindowOptions(mainWindowOptions);
-            launchHTMLApp(options, new AckListener() {
-               @Override
-               public void onSuccess(Ack ack) {
-                  Application app = (Application) ack.getSource();
-                  try {
-                     Thread.sleep(1000);
-                     embedStartupApp(app);
-                  } catch (Exception ex) {
-                     ex.printStackTrace();
-                  }
+      try {
+         String uuid = UUID.randomUUID().toString();
+         ApplicationOptions options = new ApplicationOptions(uuid, uuid, openfin_app_url);
+         WindowOptions mainWindowOptions = new WindowOptions();
+         mainWindowOptions.setAutoShow(true);
+         mainWindowOptions.setFrame(false);
+         mainWindowOptions.setResizable(false);
+         mainWindowOptions.setDefaultHeight(height);
+         mainWindowOptions.setDefaultTop(50);
+         mainWindowOptions.setDefaultWidth(width);
+         mainWindowOptions.setShowTaskbarIcon(true);
+         mainWindowOptions.setSaveWindowState(false);  // set to false so all windows start at same initial positions for each run
+         options.setMainWindowOptions(mainWindowOptions);
+         launchHTMLApp(options, new AckListener() {
+            @Override
+            public void onSuccess(Ack ack) {
+               Application app = (Application) ack.getSource();
+               try {
+                  embedStartupApp(app,frame);
+               } catch (Exception ex) {
+                  ex.printStackTrace();
                }
-               @Override
-               public void onError(Ack ack) {
-                  // logger.error(String.format("Error launching %s %s", options.getUUID(), ack.getReason()));
-               }
-            });
-         } catch (Exception e) {
-            // logger.error("Error launching app", e);
-         }
+            }
+            @Override
+            public void onError(Ack ack) {
+               // logger.error(String.format("Error launching %s %s", options.getUUID(), ack.getReason()));
+            }
+         });
+      } catch (Exception e) {
+         // logger.error("Error launching app", e);
       }
+   }
 
    private void launchHTMLApp(ApplicationOptions options, AckListener ackListener) throws Exception {
       //logger.debug(String.format("Launching %s", options.getUUID()));
@@ -129,13 +235,13 @@ public class DesktopFrame extends JFrame
             }
 
             private void onRuntimeReady() {
-              // addMenu.setEnabled(true);
+               // addMenu.setEnabled(true);
                launchHTMLApps();
 
             }
 
             @Override
-            public void onClose(String error) {
+            public void onClose() {
                // updateMessagePanel(String.format("Connection closed %s", error));
             }
             @Override
@@ -168,7 +274,9 @@ public class DesktopFrame extends JFrame
    protected String startupUuid = "OpenFinHelloWorld";
    protected java.awt.Canvas embedCanvas;
    protected Long previousPrarentHwndId;
-   private void embedStartupApp(Application app) {
+   JFrame hiddenFrame;
+   JInternalFrame frame;
+   private void embedStartupApp(Application app,JInternalFrame frame) {
       try {
          if (app == null) {
             app = Application.wrap(this.startupUuid, this.desktopConnection);
@@ -180,6 +288,7 @@ public class DesktopFrame extends JFrame
          html5Wnd.embedInto(parentHWndId, this.embedCanvas.getWidth(), this.embedCanvas.getHeight(), new AckListener() {
             @Override
             public void onSuccess(Ack ack) {
+               frame.setIconifiable(true);
                if (ack.isSuccessful()) {
                   previousPrarentHwndId = ack.getJsonObject().getLong("hWndPreviousParent");
                } else {
@@ -188,10 +297,81 @@ public class DesktopFrame extends JFrame
             }
             @Override
             public void onError(Ack ack) {
+               frame.setIconifiable(true);
             }
          });
       } catch (Exception e) {
          e.printStackTrace();
       }
    }
+
+   private class BTSInofied extends AbstractAction {
+      JInternalFrame frame;
+      public BTSInofied(JInternalFrame frame)   {
+         super();
+         this.frame = frame;
+      }
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+         if(frame.isIconifiable()) {
+            if(!frame.isIcon()) {
+               long hiddenFrameHWndId = Native.getComponentID(hiddenFrame);
+               if(startupHtml5app!=null) {
+                  startupHtml5app.getWindow().embedInto(hiddenFrameHWndId, 640, 480, new AckListener() {
+                     @Override
+                     public void onSuccess(Ack ack) {
+                        try {
+                           frame.setIcon(true);
+                        } catch (PropertyVetoException e) {
+                           e.printStackTrace();
+                        }
+                     }
+
+                     @Override
+                     public void onError(Ack ack) {
+                        try {
+                           frame.setIcon(true);
+                        } catch (PropertyVetoException e1) {
+                           e1.printStackTrace();
+                        }
+                     }
+                  });
+               }
+
+            } else{
+               try { frame.setIcon(false); } catch (PropertyVetoException e1) { }
+            }
+         }
+      }
+   }
+
+   private class NoFocusButton extends JButton {
+      private String uiKey;
+      public NoFocusButton(String uiKey, String opacityKey) {
+         setFocusPainted(false);
+         setMargin(new Insets(0,0,0,0));
+         this.uiKey = uiKey;
+
+         Object opacity = UIManager.get(opacityKey);
+         if (opacity instanceof Boolean) {
+            setOpaque(((Boolean)opacity).booleanValue());
+         }
+      }
+      public boolean isFocusTraversable() { return false; }
+      public void requestFocus() {}
+      public AccessibleContext getAccessibleContext() {
+         AccessibleContext ac = super.getAccessibleContext();
+         if (uiKey != null) {
+            ac.setAccessibleName(UIManager.getString(uiKey));
+            uiKey = null;
+         }
+         return ac;
+      }
+   }  // end NoFocusButton
+
 }
+
+
+
+
